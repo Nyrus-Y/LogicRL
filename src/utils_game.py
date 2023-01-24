dc993f662c351c68c692af18983a496f31ccd99c
Nyrus-Y <tongzhiyang0723@gmail.com>
2023-01-24 21:17:09 +0100

    log data update

--- src/utils_game.py
+++ src/utils_game.py
@@ -9,9 +9,18 @@ from environments.procgen.procgen import ProcgenGym3Env
 from environments.coinjump.coinjump.imageviewer import ImageViewer
 from environments.coinjump.coinjump.coinjump.paramLevelGenerator import ParameterizedLevelGenerator
 from environments.coinjump.coinjump.coinjump.coinjump import CoinJump
+from environments.coinjump.coinjump.coinjump.actions import CoinJumpActions
 
 
-def render_coinjump(agent, args):
+def render_getout(agent, args):
+    KEY_SPACE = 32
+    # KEY_SPACE = 32
+    KEY_w = 119
+    KEY_a = 97
+    KEY_s = 115
+    KEY_d = 100
+    KEY_r = 114
+
     def setup_image_viewer(coinjump):
         viewer = ImageViewer(
             "coinjump",
@@ -44,17 +53,24 @@ def render_coinjump(agent, args):
     target_frame_duration = 1 / fps
     last_frame_time = 0
 
-    num_epi = 0
+    num_epi = 1
     max_epi = 100
     total_reward = 0
     epi_reward = 0
+    current_reward = 0
     step = 0
     last_explaining = None
     if args.log:
-        log_f = open(args.logfile, "w+")
-        writer = csv.writer(log_f)
-        head = ['episode', 'step', 'reward', 'logic_state', 'probs']
-        writer.writerow(head)
+        if args.alg == 'logic':
+            log_f = open(args.logfile, "w+")
+            writer = csv.writer(log_f)
+            head = ['episode', 'step', 'reward', 'average_reward', 'logic_state', 'probs']
+            writer.writerow(head)
+        else:
+            log_f = open(args.logfile, "w+")
+            writer = csv.writer(log_f)
+            head = ['episode', 'step', 'reward', 'average_reward']
+            writer.writerow(head)
 
     while num_epi <= max_epi:
         # control framerate
@@ -67,23 +83,49 @@ def render_coinjump(agent, args):
         last_frame_time = current_frame_time  # save frame start time for next iteration
         # step game
         step += 1
+        action = []
         if not coin_jump.level.terminated:
             if args.alg == 'logic':
                 action, explaining = agent.act(coin_jump)
-            else:
+            elif args.alg == 'ppo':
                 action = agent.act(coin_jump)
+            elif args.alg == 'human':
+                if KEY_a in viewer.pressed_keys:
+                    action.append(CoinJumpActions.MOVE_LEFT)
+                if KEY_d in viewer.pressed_keys:
+                    action.append(CoinJumpActions.MOVE_RIGHT)
+                if (KEY_SPACE in viewer.pressed_keys) or (KEY_w in viewer.pressed_keys):
+                    action.append(CoinJumpActions.MOVE_UP)
+                if KEY_s in viewer.pressed_keys:
+                    action.append(CoinJumpActions.MOVE_DOWN)
         else:
-            coin_jump = create_coinjump_instance(args)
-            print("epi_reward: ", round(epi_reward, 2))
-            print("--------------------------     next game    --------------------------")
-            total_reward += epi_reward
-            epi_reward = 0
-            action = 0
-            num_epi += 1
-            step = 0
+            if args.alg == 'human':
+                if KEY_r in viewer.pressed_keys:
+                    coin_jump = create_coinjump_instance(args)
+                    # print("epi_reward: ", round(epi_reward, 2))
+                    print("--------------------------     next game    --------------------------")
+                    total_reward += epi_reward
+                    epi_reward = 0
+                    action = 0
+                    # average_reward = round(total_reward / num_epi, 2)
+                    num_epi += 1
+                    step = 0
+
+            else:
+                coin_jump = create_coinjump_instance(args)
+                # print("epi_reward: ", round(epi_reward, 2))
+                print("--------------------------     next game    --------------------------")
+                total_reward += epi_reward
+                epi_reward = 0
+                action = 0
+                # average_reward = round(total_reward / num_epi, 2)
+                num_epi += 1
+                step = 0
 
         reward = coin_jump.step(action)
-
+        score = coin_jump.get_score()
+        current_reward += reward
+        average_reward = round(current_reward / num_epi, 2)
         if args.alg == 'logic':
             if last_explaining is None:
                 print(explaining)
@@ -93,12 +135,17 @@ def render_coinjump(agent, args):
                 last_explaining = explaining
 
         if args.log:
-            probs = agent.get_probs()
-            logic_state = agent.get_state(coin_jump)
-            data = [(num_epi, step, reward, logic_state, probs)]
-            writer.writerows(data)
+            if args.alg == 'logic':
+                probs = agent.get_probs()
+                logic_state = agent.get_state(coin_jump)
+                data = [(num_epi, step, reward, average_reward, logic_state, probs)]
+                writer.writerows(data)
+            else:
+                data = [(num_epi, step, reward, average_reward)]
+                writer.writerows(data)
         # print(reward)
         epi_reward += reward
+
         if args.render:
             np_img = np.asarray(coin_jump.camera.screen)
             viewer.show(np_img[:, :, :3])
@@ -112,7 +159,8 @@ def render_coinjump(agent, args):
         if coin_jump.level.terminated:
             step = 0
             print(num_epi)
-        if num_epi == 100:
+            print('reward: ' + str(round(score, 2)))
+        if num_epi > 100:
             break
 
     print('total reward= ', total_reward)
@@ -127,10 +175,16 @@ def render_bigfish(agent, args):
     env = ProcgenGym3Env(num=1, env_name=args.env, render_mode="rgb_array")
 
     if args.log:
-        log_f = open(args.logfile, "w+")
-        writer = csv.writer(log_f)
-        head = ['episode', 'step', 'reward', 'logic_state', 'probs']
-        writer.writerow(head)
+        if args.alg == 'logic':
+            log_f = open(args.logfile, "w+")
+            writer = csv.writer(log_f)
+            head = ['episode', 'step', 'reward', 'average_reward', 'logic_state', 'probs']
+            writer.writerow(head)
+        else:
+            log_f = open(args.logfile, "w+")
+            writer = csv.writer(log_f)
+            head = ['episode', 'step', 'reward', 'average_reward']
+            writer.writerow(head)
 
     if agent == "human":
         ia = gym3.Interactive(env, info_key="rgb", height=768, width=768)
@@ -140,9 +194,10 @@ def render_bigfish(agent, args):
             env = gym3.ViewerWrapper(env, info_key="rgb")
         reward, obs, done = env.observe()
         total_r = 0
-        epi = 0
+        epi = 1
         epi_r = 0
         step = 0
+        current_r = 0
         last_explaining = None
         while True:
             # print(obs['positions'])
@@ -154,7 +209,9 @@ def render_bigfish(agent, args):
             rew, obs, done = env.observe()
             total_r += rew[0]
             epi_r += rew[0]
-
+            current_r += rew[0]
+            step += 1
+            average_r = round(current_r / epi, 2)
             if args.alg == 'logic':
                 if last_explaining is None:
                     print(explaining)
@@ -164,10 +221,14 @@ def render_bigfish(agent, args):
                     last_explaining = explaining
 
             if args.log:
-                probs = agent.get_probs()
-                logic_state = agent.get_state(obs)
-                data = [(epi, step, rew[0], logic_state, probs)]
-                writer.writerows(data)
+                if args.alg == 'logic':
+                    probs = agent.get_probs()
+                    logic_state = agent.get_state(obs)
+                    data = [(epi, step, rew[0], average_r, logic_state, probs)]
+                    writer.writerows(data)
+                else:
+                    data = [(epi, step, rew[0], average_r)]
+                    writer.writerows(data)
 
             if done:
                 epi += 1
@@ -176,7 +237,7 @@ def render_bigfish(agent, args):
                 print("reward: ", epi_r)
                 epi_r = 0
 
-            if epi == 100:
+            if epi > 100:
                 break
 
         print('total reward= ', total_r)
@@ -189,10 +250,16 @@ def render_heist(agent, args):
     env = ProcgenGym3Env(num=1, env_name=args.env, render_mode="rgb_array")
 
     if args.log:
-        log_f = open(args.logfile, "w+")
-        writer = csv.writer(log_f)
-        head = ['episode', 'step', 'reward', 'logic_state', 'probs']
-        writer.writerow(head)
+        if args.alg == 'logic':
+            log_f = open(args.logfile, "w+")
+            writer = csv.writer(log_f)
+            head = ['episode', 'step', 'reward', 'average_reward', 'logic_state', 'probs']
+            writer.writerow(head)
+        else:
+            log_f = open(args.logfile, "w+")
+            writer = csv.writer(log_f)
+            head = ['episode', 'step', 'reward', 'average_reward']
+            writer.writerow(head)
 
     if agent == "human":
         ia = gym3.Interactive(env, info_key="rgb", height=768, width=768)
@@ -205,11 +272,12 @@ def render_heist(agent, args):
         total_r = 0
         epi_r = 0
         epi = 1
+        current_r = 0
         last_explaining = None
         while True:
             step += 1
             if args.alg == 'logic':
-                action,explaining = agent.act(obs)
+                action, explaining = agent.act(obs)
             else:
                 action = agent.act(obs)
 
@@ -217,6 +285,8 @@ def render_heist(agent, args):
             rew, obs, done = env.observe()
             total_r += rew[0]
             epi_r += rew[0]
+            current_r += rew[0]
+            average_r = round(current_r / epi, 2)
             if args.alg == 'logic':
                 if last_explaining is None:
                     print(explaining)
@@ -226,10 +296,14 @@ def render_heist(agent, args):
                     last_explaining = explaining
 
             if args.log:
-                probs = agent.get_probs()
-                logic_state = agent.get_state(obs)
-                data = [(epi, step, rew[0], logic_state, probs)]
-                writer.writerows(data)
+                if args.alg == 'logic':
+                    probs = agent.get_probs()
+                    logic_state = agent.get_state(obs)
+                    data = [(epi, step, rew[0], average_r, logic_state, probs)]
+                    writer.writerows(data)
+                else:
+                    data = [(epi, step, rew[0], average_r)]
+                    writer.writerows(data)
 
             if done:
                 epi += 1
@@ -237,7 +311,7 @@ def render_heist(agent, args):
                 print("episode: ", epi)
                 print("reward: ", epi_r)
                 epi_r = 0
-            if epi == 100:
+            if epi > 100:
                 break
 
         print('total reward= ', total_r)
